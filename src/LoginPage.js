1291830bc75d0a2f0a198393ace7af604ebf2049
Torsu Derrick <torsuderrick2004@gmail.com>
2023-12-13 16:29:29 +0000

    second commit

--- src/LoginPage.js
+++ src/LoginPage.js
@@ -1,9 +1,9 @@
 // LoginPage.js
 
-import React, { useState } from 'react';
-import styled from 'styled-components';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
+import React, { useState } from "react";
+import styled from "styled-components";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEye, faEyeSlash } from "@fortawesome/free-solid-svg-icons";
 import "./index.css";
 
 const Container = styled.div`
@@ -17,7 +17,7 @@ const Panel = styled.div`
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  background-color: ${(props) => props.backgroundColor || '#ffffff'};
+  background-color: ${(props) => props.backgroundColor || "#ffffff"};
 `;
 
 const LoginForm = styled.form`
@@ -40,27 +40,12 @@ const FormGroup = styled.div`
   position: relative;
 `;
 
-const FormInput = styled.input`
-  width: calc(100% - 20px);
-  padding: 10px;
-  border: 1px solid #ddd;
-  border-radius: 4px;
-  margin-bottom: 10px;
-  box-sizing: border-box;
-`;
-
-const EyeIcon = styled(FontAwesomeIcon)`
-  position: absolute;
-  top: 50%;
-  right: 10px;
-  transform: translateY(-50%);
-  cursor: pointer;
-  color: #666;
-`;
+const EyeIcon = styled(FontAwesomeIcon)``;
 
 const FormLabel = styled.label`
   display: block;
   margin-bottom: 5px;
+  colour: #757575;
 `;
 
 const SubmitButton = styled.button`
@@ -99,37 +84,69 @@ const LoginPage = () => {
     setShowPassword((prevShowPassword) => !prevShowPassword);
   };
 
+  const [onBlur, setOnBlur] = useState(false);
+  const [onBlur2, setOnBlur2] = useState(false);
+
   return (
-    <Container>
-      <Panel backgroundColor="#ffffff">
-        <LoginForm className="leftPanel">
-          <SignInHeader>Sign In</SignInHeader>
-          <FormGroup>
-            <FormLabel htmlFor="staffEmail">E-mail</FormLabel>
-            <FormInput type="text" id="staffEmail" placeholder="username@slcb.com"/>
-          </FormGroup>
-          <FormGroup>
-            <FormLabel htmlFor="password">Password</FormLabel>
-            <FormInput
-              type={showPassword ? 'text' : 'password'}
-              id="password"
-              placeholder="Password"
-            />
-            <EyeIcon
-              icon={showPassword ? faEyeSlash : faEye}
-              onClick={togglePasswordVisibility}
-            />
-          </FormGroup>
-          <SubmitButton type="submit">Sign In</SubmitButton>
-        </LoginForm>
-      </Panel>
-      <AccentLine className="accentLine" />
-      <Panel backgroundColor="#fffff">
-        <RightPanel className="rightpanel">
-          <Image src="slcb_logo_nobg.png" alt="SLCB Logo" />
-        </RightPanel>
-      </Panel>
-    </Container>
+    <div className="containers">
+      <Container>
+        <Panel backgroundColor="#ffffff">
+          <LoginForm className="leftPanel">
+            <SignInHeader>Sign In</SignInHeader>
+            <FormGroup>
+              <FormLabel htmlFor="staffEmail">E-mail</FormLabel>
+              <div
+                style={{
+                  border: onBlur2 ? "2px solid red" : "2px solid #cbd5e0",
+                  borderRadius: "10px",
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "space-between",
+                  padding: "7px",
+                  backgroundColor: "#e8f0fe",
+                }}
+                onBlur={() => setOnBlur2(false)}
+                onFocus={() => setOnBlur2(true)}
+              >
+                <input />
+              </div>
+            </FormGroup>
+            <FormGroup>
+              <FormLabel htmlFor="password">Password</FormLabel>
+              <div
+                style={{
+                  border: onBlur ? "2px solid #c80815" : "2px solid #cbd5e0",
+                  borderRadius: "10px",
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "space-between",
+                  padding: "7px",
+                  backgroundColor: "#e8f0fe",
+                }}
+                onBlur={() => setOnBlur(false)}
+                onFocus={() => setOnBlur(true)}
+              >
+                <input type={showPassword ? "text" : "password"} />
+                <EyeIcon
+                  icon={showPassword ? faEyeSlash : faEye}
+                  onClick={togglePasswordVisibility}
+                />
+              </div>
+            </FormGroup>
+            <br />
+            <SubmitButton type="submit">Sign In</SubmitButton>
+          </LoginForm>
+        </Panel>
+        <AccentLine className="accentLine" />
+        <Panel backgroundColor="#fffff">
+          <div className="rightpanel">
+            <RightPanel>
+              <Image src="slcb_logo_nobg.png" alt="SLCB Logo" />
+            </RightPanel>
+          </div>
+        </Panel>
+      </Container>
+    </div>
   );
 };
 
